8816a2ff0f5edd3913493db6c933b0b986f3d7e2
hao365 <fq.gao@hao365.com>
2013-03-27 16:30:45 +0800

    Update test.php

--- test.php
+++ test.php
@@ -6,11 +6,12 @@ $arr = array(
     array('abc', 'ab', 'abd', 'bcd', 'aca', 'bca', 'bc'),
     array('xxxy', 'xy', 'yyy', 'x', 'y'),
     array('北京', '京', '上', '上海'),
-    array('b', 'ab')
+    array('b', 'ab'),
+    array('a', 'ab')
 );
 
 foreach ($arr as $a) {
-    $m = new SimpleRegexpBuilder($a);
+    $m = new SimpleRegexpBuilder($a, array('b' => false));
     $reg = $m->get();
     echo $reg . PHP_EOL;
 }
