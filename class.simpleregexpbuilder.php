c1096c7475693819d3d48f848ededb3e1b7cfc33
Gao Faqiang <fq.gao@hao365.com>
2013-03-27 17:26:28 +0800

    I don't know

--- class.simpleregexpbuilder.php
+++ class.simpleregexpbuilder.php
@@ -2,39 +2,39 @@
 
 class SimpleRegexpBuilder {
 
-    private $pattern = '';
-    private $opts = array('i' => true, 'encoding' => 'utf8', 'delimiter' => '/', 'b' => true); /* i: not case sensitive, encoding: string in array encoding, delimiter: start and end character, b: bounary */
-    private $_u = ''; # i18n need
+    protected $pattern = '';
+    public $opts = array('i' => true, 'encoding' => 'utf8', 'delimiter' => '/', 'b' => true); /* i: not case sensitive, encoding: string in array encoding, delimiter: start and end character, b: bounary */
+    protected $_u = ''; # i18n need
 
     public function __construct(array $words, array $opts = array()) {
         $this->opts = $opts + $this->opts;
         $this->pattern = $this->opts['delimiter'] . ($this->opts['b'] ? '\b' : '') . substr($this->g(call_user_func_array('array_merge_recursive', array_map(array($this, 't'), $words))), 3, -1) . ($this->opts['b'] ? '\b' : '') . $this->opts['delimiter'] . $this->_u . ($this->opts['i'] ? 'i' : '') . 'S';
     }
 
-    private function t($str) {
+    protected function t($str) {
         $c = mb_substr($str, 0, 1, $this->opts['encoding']);
         if (strlen($c) > 1)
             $this->_u = 'u';
         else if ($this->opts['i'])
             $c = strtolower($c);
-        return array($c => mb_strlen($str, $this->opts['encoding']) > 1 ? $this->t(mb_substr($str, 1, mb_strlen($str, $this->opts['encoding']), $this->opts['encoding'])) : array('NULL' => ''));
+        return array($c => mb_strlen($str, $this->opts['encoding']) > 1 ? $this->t(mb_substr($str, 1, mb_strlen($str, $this->opts['encoding']), $this->opts['encoding'])) : array('' => ''));
     }
 
-    private function g(array $tree, $level = 0) {
+    protected function g(array $tree, $level = 0) {
         $ret = array();
         foreach ($tree as $node => $cnode)
-            if ($node != 'NULL')
-                $ret[] = $node . ($cnode == array('NULL' => '') ? '' : $this->g($cnode, $level + 1));
-        $b = isset($tree['NULL']);
+            if ($node != '')
+                $ret[] = $node . ($cnode == array('' => '') ? '' : $this->g($cnode, $level + 1));
+        $b = isset($tree['']);
         return ((count($ret) > 1 || (mb_strlen($ret[0], $this->opts['encoding']) > 1 && $b) || !$level ? '(?:' . join(($this->opts['b'] && !$level ? '\b|\b' : '|'), $ret) . ')' : $ret[0]) . ($b ? '?' : ''));
     }
 
-    public function get() {
+    public function getRegexp() {
         return $this->pattern;
     }
 
     public function match($str, &$m) {
-        return preg_match($this->pattern, $str, $r) ? count($m = $r[0]) : 0;
+        return preg_match($this->pattern, $str, $m);
     }
 
     public function matchAll($str, array &$m) {
--- test.php
+++ test.php
@@ -6,11 +6,28 @@ $arr = array(
     array('abc', 'ab', 'abd', 'bcd', 'aca', 'bca', 'bc'),
     array('xxxy', 'xy', 'yyy', 'x', 'y'),
     array('北京', '京', '上', '上海'),
-    array('b', 'ab')
+    array('b', 'ab'),
+    array('a', 'ab')
 );
 
 foreach ($arr as $a) {
-    $m = new SimpleRegexpBuilder($a);
-    $reg = $m->get();
-    echo $reg . PHP_EOL;
+    $m = new SimpleRegexpBuilder($a, array('b' => false));
+    echo $m->getRegexp() . PHP_EOL;
 }
+
+echo PHP_EOL;
+echo PHP_EOL;
+echo PHP_EOL;
+
+echo '============ INPUT ============' . PHP_EOL;
+$keywords = array('fox', 'fax', 'god', 'dog', 'doggie', 'abc', 'hover', 'the', 've');
+$text = 'The quick brown fox jumps over a lazy dog';
+$m = new SimpleRegexpBuilder($keywords, array('i' => true, 'b' => true));
+echo "Text:\t\t" . $text . PHP_EOL;
+echo "List:\t\t" . join(' ', $keywords) . PHP_EOL;
+echo "Regexp:\t\t" . $m->getRegexp() . PHP_EOL;
+echo "Options:" . var_export($m->opts, 1) . PHP_EOL . PHP_EOL;
+$matches = array();
+echo '============ OUTPUT ===========' . PHP_EOL;
+echo "Count:\t\t" . $m->matchAll($text, $matches) . PHP_EOL;
+echo "Result:" . print_r($matches, 1) . PHP_EOL;
